c72a6005ed774d2c848745487abd075b64dc2805
SergioLuiz10 <sergioluizteixeira12345@gmail.com>
2024-12-17 21:56:25 -0300

    figura dos dois países feita

--- grafico.py
+++ grafico.py
@@ -19,5 +19,15 @@ plt.xlabel("Ano de imigração")
 plt.ylabel("Número de imigrantes") 
 plt.xticks(["1980", "1985", "1990", "1995", "2000", "2005", "2010"])
 plt.legend(loc="upper left") 
+
+fig, ax = plt.subplots(figsize=(10, 6)) 
+ax.plot(dfAr["anoImiAr"], dfAr["qtdImiAR"], label="Argentina", color="blue", marker='o')
+ax.plot(dfBra["anoImiBra"], dfBra["qtdImiBra"], label="Brasil", color="red", marker='o')
+ax.set_title("Imigração Argentina e Brasil para o Canadá")
+ax.set_xlabel("Ano de imigração")
+ax.set_ylabel("Número de imigrantes")
+ax.set_xticks(years[::5]) 
+ax.tick_params(axis='x', rotation=45)  
+
 plt.show()
 
